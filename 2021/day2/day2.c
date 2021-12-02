f2f847cc611bb8b84bac814c1a04dcd04d017a8b
Stoffe <you@example.com>
2021-12-02 10:57:24 +0100

    day 2 written in C

---
+++ 2021/day2/day2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+struct Position
+{
+    int Horizontal;
+    int Depth;
+    int DepthAim;
+    int Aim;
+};
+
+int main()
+{
+    FILE *fp;
+    struct Position pos;
+    pos.Depth = 0;
+    pos.DepthAim = 0;
+    pos.Horizontal = 0;
+    pos.Aim = 0;
+
+    fp = fopen("input.txt", "r");
+
+    char op[10];
+    while (fscanf(fp, "%s", op) == 1)
+    {
+        int c;
+
+        fscanf(fp, "%d", &c);
+
+        if (op[0] == 'f')
+        {
+            pos.Horizontal += c;
+            pos.DepthAim += pos.Aim * c;
+        }
+        else if (op[0] == 'd')
+        {
+            pos.Aim += c;
+            pos.Depth += c;
+
+        }
+        else if (op[0] == 'u')
+        {
+            pos.Aim -= c;
+            pos.Depth -= c;
+
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    fprintf(stdout, "part 1: %d \n", pos.Depth * pos.Horizontal);
+    fprintf(stdout, "part 2: %d \n", pos.DepthAim * pos.Horizontal);
+}
