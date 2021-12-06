059a5286d63cbb7788c566c59734a73b7daecfae
Stoffe <you@example.com>
2021-12-06 19:30:44 +0100

    search

--- 2021/day3/day3_test.go
+++ 2021/day3/day3_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -44,4 +47,50 @@ func TestPart2(t *testing.T) {
 
 	assert.Equal(t, oxygen, 23)
 	assert.Equal(t, co2, 10)
+}
+
+// fan va coolt, har lyckats implementera en sorterinsalgoritm :-D
+// problemet just nu är att den root är den största biten.
+// Detta gör att siffor med olika antal bits hamnar fel.
+// Exempel:
+// 101  8
+// 10   4
+// för att det ska gå så behöver 10 ha skrivas om till 010
+func TestSort(t *testing.T) {
+	testinput := `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`
+
+	input := strings.Split(testinput, "\n")
+
+	root := node{}
+	for _, item := range input {
+		i, err := strconv.ParseInt(item, 2, 0)
+		bitCount := len(item)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(" %d", i)
+		root.insert(int(i), bitCount)
+	}
+
+	fmt.Println()
+	res := root.Out()
+
+	for i := 1; i < len(res); i++ {
+		assert.GreaterOrEqual(t, res[i], res[i-1])
+	}
+
+	fmt.Print(res)
+	fmt.Println()
 }
