58325055898da5ce42af4ef2cef509f24f353c8a
Stoffe <you@example.com>
2021-12-07 11:29:32 +0100

    day7

--- 2021/day7/day7_test.go
+++ 2021/day7/day7_test.go
@@ -7,9 +7,18 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	fuel := part1(testinput)
+	fuel, _ := day7(testinput)
 
 	assert.Equal(t, 37, fuel)
+}
+
+// dagens var inte jättebra..
+// testdatan ska rundas uppåt mean = 4.9 ska bli 5
+// medans riktiga ska rundas nedåt, 464.567 blir 464
+func TestPart2(t *testing.T) {
+	_, fuel := day7(input)
+
+	assert.Equal(t, 168, fuel)
 }
 
 var testinput = "16,1,2,0,4,2,7,1,2,14"
